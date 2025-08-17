d72321ad24db331d882eb88f0b0a4b7a5f60792d
Reed <coffeyreed@proton.me>
2025-08-17 06:31:24 -0700

    Hamburger to all pages

--- javascript/header.js
+++ javascript/header.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.body.insertAdjacentHTML("afterbegin", `
+    
     <div class="ignore-on-mobile">
       <div class="header">
         <ul class="title-content-list">
@@ -29,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
       <div class="topnav">
-        <a href="#home" class="active">Logo</a>
+        <a href="index.html" class="active" style="font-size:50px">RC Art</a>
         <div id="myLinks" style="display:none;">
           <a href="recent_work.html">Recent Work</a>
           <a href="agaves.html">Agave Studies</a>
