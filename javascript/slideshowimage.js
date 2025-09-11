a40dbce0b175832ef8ecd22e026a1f7a0a4e04a5
Reed <coffeyreed@proton.me>
2025-09-11 15:04:29 -0700

    Sick of the buttons, bye

--- javascript/slideshowimage.js
+++ javascript/slideshowimage.js
@@ -1,7 +1,7 @@
 $.getJSON("/recent_work/images.json", function(files) {
     files.forEach(function(file) {
         $("#gallery").append(
-            `<img src='/recent_work/${file}' style='height:auto; width:50vw; display:block; transition:.5s ease; backface-visibility:hidden; padding-bottom:45px;'>`
+            `<img src='/recent_work/${file}' style='height:500vw; width:auto; display:block; transition:.5s ease; backface-visibility:hidden; padding-bottom:45px;'>`
         );
     });
 });
