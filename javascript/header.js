950a6c3dee0ec7d27a835697277da0ccd845185a
Reed <coffeyreed@proton.me>
2025-09-14 10:35:00 -0700

    Link to index

--- javascript/header.js
+++ javascript/header.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
       <div class="header">
         <ul class="title-content-list">
           <li>
-             <a href="index.html">
+             <a href="reedcoffey.art">
   <img src="images/Logo1.png" alt="HTML tutorial" style="width:auto;height:5vw;margin-bottom:-10%;">
 </a> 
             <div class="content">
@@ -29,9 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
       <link rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <div class="topnav">
-         <a href="index.html">
+         <a href="reedcoffey.art">
            <img src="images/Logo1.png" alt="HTML tutorial" style="height:10vw;">
          </a> 
+
         <div id="myLinks" style="display:none;">
           <a href="oilnav.html">Oil Paintings</a>
           <a href="recent_work.html">Recent Work</a>
