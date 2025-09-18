d8d4782f6ff2cc82a9af59cd9927201baa2b5eea
Reed <coffeyreed@proton.me>
2025-09-17 21:05:29 -0700

    Adding about page

--- javascript/slideshowimage.js
+++
@@ -1,7 +0,0 @@
-$.getJSON("/recent_workimages.json", function(files) {
-    files.forEach(function(file) {
-        $("#gallery").append(
-            `<img src='/recent_work/${file}' style='height:500vw; width:auto; display:block; transition:.5s ease; backface-visibility:hidden; padding-bottom:45px;'>`
-        );
-    });
-});
