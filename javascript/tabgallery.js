0e44e70b552bf8d438a375756a428814c4792ad2
Reed <coffeyreed@proton.me>
2025-11-01 18:03:36 -0700

    maybe add swipe?

--- javascript/tabgallery.js
+++ javascript/tabgallery.js
@@ -81,3 +81,65 @@ document.addEventListener('DOMContentLoaded', () => {
     showImage(currentIndex + 1);
   });
 });
+
+function swipeDetect(el, callback){
+  var touchsurface = el,
+  swipDir,
+  startX,
+  startY,
+  distX,
+  distY,
+  threshold = 150,
+  restraint = 100,
+  allowedTime = 300,
+  elapsedTime,
+  startTime,
+  handleswipe = callback || function(swipeDir){}
+
+  touchsurface.addEventListener('touchstart', function(e){
+    var touchobj = e.changedTouches[0]
+    swipeDir = 'none'
+    dist = 0
+    startX = touchobj.pageX
+    startY = touchobj.pageY
+    startTime = new Date().getTime()
+    e.preventDefault()
+  }, false)
+
+  touchsurface.addEventListener('touchmove', function(e){
+    e.preventDefault()
+  }, false)
+
+  touchsurface.addEventListener('touchend', function(e){
+    var touchobj = e.changedTouches[0]
+    distX = touchobj.pageX - startX
+    distY = touchobj.pageY - startY
+    elapsedTime = new Date().getTime() - startTime
+    if (elapsedTime <= allowedTime){
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+        swipDir = (distX < 0)? 'left' : 'right'
+      }
+      else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){
+        swipDir = (distY < 0)? 'up' : 'down'
+      }
+    }
+    handleswipe(swipDir)
+    e.preventDefault()
+  }, false)
+}
+
+function mobileConSOFF(){
+  if (isMobile){
+    let bunny = document.querySelector('#board')
+    swipeDetect(bunny, function(swipDir){
+      DataTransferItemList.forEach(tile => {
+        tile.classList.remove("smushed")
+      })
+      if(swipeDir == 'left'){
+        showImage(currentIndex - 1)
+      } else if(swipeDetect == 'right'){
+        showImage(currentIndex + 1)
+      }
+    })
+  }
+}
