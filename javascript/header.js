3561d45534b64b34a36c936cbe84cee82932dbda
Reed <coffeyreed@proton.me>
2026-04-16 19:32:50 -0700

    Tweaking fonts

--- javascript/header.js
+++ javascript/header.js
@@ -2,10 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   document.body.insertAdjacentHTML("afterbegin", `
     
 
-<a href="index.html" style="text-decoration: none;">
-  <p style="text-align: center; font-size: 40pt; color: #53784F;">
-    REED COFFEY
-  </p>
+<a href="index.html" class="site-title-link">
+  <h1 class="site-title">REED COFFEY</h1>
 </a>
       <div class="header">
         <div class="center">
@@ -13,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <li>
             <div class="content">
               <div class="dropdown" style="z-index:11">
-                <button class="dropbtn"><a href="oilnav.html">Oil Paintings</a></button>
+                <a href="oilnav.html" class="dropbtn">Oil Paintings</a>
                 <div class="dropdown-content">
                   <a href="recent_work.html">Recent Work</a>
                   <a href="agaves.html">Agave Studies</a>
