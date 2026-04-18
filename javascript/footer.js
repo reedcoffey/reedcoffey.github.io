9463b1fde7781030d44bda6d52ceb7e7876e9382
Reed <coffeyreed@proton.me>
2026-04-17 20:29:24 -0700

    Fancy fancy

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,16 +1,16 @@
 document.body.insertAdjacentHTML('beforeend', `
 <footer>
  <div class="ignore-on-mobile">
-  <div class="title" style="font-size: 20px; margin-top: 12vw;">
+  <div class="title" style="font-size: 20px; padding-top:5vh; padding-bottom: 2vh;">
     <a href="default.asp">
            <img src="images/Logo1.png" alt="HTML tutorial" style="height:04vw;">
          </a> 
   </div>
     <a href="https://bsky.app/profile/throughthereeds.bsky.social">
-      <img src="images/Bsky.png" alt="Bluesky" style="width:autox;height:42px; margin-right:-15px;">
+      <img src="images/Bsky.png" alt="Bluesky" style="width:autox;height:42px; margin-right:-15px; padding-bottom:5vh; padding-right:1vw;">
     </a>
         <a href="mailto:coffeyreed@proton.me">
-      <img src="images/Mail_logo.png" alt="Bluesky" style="width:auto;height:42px;margin-left:-15px;"> 
+      <img src="images/Mail_logo.png" alt="Bluesky" style="width:auto;height:42px;margin-left:-15px; padding-bottom:5vh; padding-left:1vw;"> 
     </a>
     </div>
 
